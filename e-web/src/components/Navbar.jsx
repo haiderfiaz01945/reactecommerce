c074fb20944926b822acf2c83f17bf3f9405260f
Abdulazizchughtai <abdulazizchughtai793@gmail.com>
2025-05-03 18:26:34 +0500

    navsideschanges

--- e-web/src/components/Navbar.jsx
+++ e-web/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 </div>
  
 
-<div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+<div className={nav ? 'fixed right-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
 
         <h1 className='text-3xl font-bold text-[#00df9a] m-4'>React.</h1>
         <ul className='uppercase'>
