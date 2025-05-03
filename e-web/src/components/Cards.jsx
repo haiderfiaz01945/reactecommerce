c573e98d7f01667fb0e6987acd61e620236465f4
Abdulazizchughtai <abdulazizchughtai793@gmail.com>
2025-05-03 18:10:27 +0500

    typoo

--- e-web/src/components/Cards.jsx
+++ e-web/src/components/Cards.jsx
@@ -9,7 +9,7 @@ const Cards = () => {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>singloo User</h2>
+              <h2 className='text-2xl font-bold text-center py-8'>singl User</h2>
               <p className='text-center text-4xl font-bold'>$149</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
