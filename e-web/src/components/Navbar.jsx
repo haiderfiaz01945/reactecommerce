c019227f1fcd82c2a086022a08b5b556b86cf395
Abdulazizchughtai <abdulazizchughtai793@gmail.com>
2025-05-03 18:45:18 +0500

    morechanges

--- e-web/src/components/Navbar.jsx
+++ e-web/src/components/Navbar.jsx
@@ -25,25 +25,35 @@ const Navbar = () => {
 </ul>
 
 
-      <div
-  onClick={handlenav}
-  className='block md:hidden cursor-pointer hover:scale-110 hover:text-[#00df9a] transition-transform duration-300'
->
-  {!nav ? <RiMenuSearchLine size={25} /> : <IoIosClose size={25} /> }
+<div className='relative'>
+  
+  <div
+    onClick={handlenav}
+    className='block md:hidden cursor-pointer hover:scale-110 hover:text-[#00df9a] transition-transform duration-300 z-50 absolute right-4 top-6'
+  >
+    {!nav ? <RiMenuSearchLine size={25} /> : <IoIosClose size={25} />}
+  </div>
+
+
+  <div className={nav 
+    ? 'fixed right-0 top-0 w-[60%] h-full border-l border-gray-900 bg-[#000300] ease-in-out duration-500 z-40' 
+    : 'fixed right-[-100%] top-0 z-40'
+  }>
+    <h1 className='text-3xl font-bold text-[#00df9a] m-4'>React.</h1>
+    <ul className='uppercase'>
+    <li className='p-4 border-b border-gray-600 hover:text-[#00df9a] focus:text-[#00df9a] active:text-[#00df9a] transition duration-300 cursor-pointer'>
+    <Link to ="/home">Home</Link>
+</li>
+
+  <li className='p-4 border-b border-gray-600 hover:text-[#00df9a] focus:text-[#00df9a] active:text-[#00df9a] transition duration-300 cursor-pointer'><Link to ="/company">Company</Link></li>
+  <li className='p-4 border-b border-gray-600 hover:text-[#00df9a] focus:text-[#00df9a] active:text-[#00df9a] transition duration-300 cursor-pointer'>Resources</li>
+  <li className='p-4 border-b border-gray-600 hover:text-[#00df9a] focus:text-[#00df9a] active:text-[#00df9a] transition duration-300 cursor-pointer'>About</li>
+  <li className='p-4 border-b border-gray-600 hover:text-[#00df9a] focus:text-[#00df9a] active:text-[#00df9a] transition duration-300 cursor-pointer'>Contact</li>
+</ul>
+
+  </div>
 </div>
- 
 
-<div className={nav ? 'fixed right-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-
-        <h1 className='text-3xl font-bold text-[#00df9a] m-4'>React.</h1>
-        <ul className='uppercase'>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
-        </ul>
-      </div>
     </div>
   );
 };
